8737fa236b48fee506f60f864826966f53c9408e
Danack <Danack@basereality.com>
2022-08-08 16:07:19 +0100

    Remove dead files.

---
+++ src/Bristolian/ApiController/Csp.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Bristolian\ApiController;
+
+use Bristolian\CSPViolation\CSPViolationStorage;
+
+use SlimAuryn\Response\JsonResponse;
+
+use VarMap\VarMap;
+
+class Csp
+{
+
+    public function get_reports_for_page(
+        VarMap $varMap,
+        CSPViolationStorage $cspViolationStorage
+    ) {
+
+        $page = (int)$_REQUEST['page'];
+
+        $count = $cspViolationStorage->getCount();
+        $reports = $cspViolationStorage->getReportsByPage($page);
+
+        $data = [
+            'count' => $count,
+            'reports' => [],//$reports
+        ];
+
+        foreach ($reports as $report) {
+            [$error, $value] = convertToValue($report);
+            $data['reports'][] = $value;
+        }
+
+        return new JsonResponse($data);
+    }
+}
