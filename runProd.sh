df0567c669f7fbacab055278614092b4156b636d
Danack <Danack@basereality.com>
2023-04-30 13:30:09 +0100

    Correct container name.

--- runProd.sh
+++ runProd.sh
@@ -6,7 +6,7 @@
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up  --build --force-recreate installer
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up  --build --force-recreate npm_build
 
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build --force-recreate varnish nginx php_fpm redis
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build --force-recreate varnish caddy php_fpm redis
 
 
 # docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build --force-recreate varnish nginx php_fpm
