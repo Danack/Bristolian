cee9c96858dddbef1f1efc8d1166c2a1a5923d96
Danack <Danack@basereality.com>
2023-04-30 13:28:57 +0100

    Add flag for git to work.

--- containers/installer/entrypoint.sh
+++ containers/installer/entrypoint.sh
@@ -6,7 +6,7 @@ set -x
 ENV_TO_USE=${ENV_DESCRIPTION:=default}
 
 echo "ENV_TO_USE is ${ENV_TO_USE}";
-
+git config --global --add safe.directory /var/app
 
 COMPOSER_TYPE=$(php src/check_composer_command.php)
 echo "composer type is ${COMPOSER_TYPE}";
