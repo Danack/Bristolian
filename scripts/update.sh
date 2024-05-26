25946d55bec93b39c167babc078805a8107e20dc
Danack <Danack@basereality.com>
2024-05-26 13:27:05 +0100

    Ignore built file and add comment to update script.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Add this to cron
+#
+#
+# */2 * * * * /usr/bin/flock -w 0 /var/home/Bristolian/Bristolian/cron.lock /bin/sh /var/home/Bristolian/Bristolian/update.sh >> /var/log/deployer/bristolian.log 2>&1
+
+
 cd /var/home/Bristolian/Bristolian
 
 git fetch
