dbb70d75c8429626c569f942fb085a134d1f493e
Danack <Danack@basereality.com>
2024-12-19 18:32:58 +0000

    Increasing test coverage.

--- test/BristolianTest/Middleware/ContentSecurityPolicyMiddlewareTest.php
+++ test/BristolianTest/Middleware/ContentSecurityPolicyMiddlewareTest.php
@@ -17,13 +17,10 @@ use Laminas\Diactoros\Response;
 use Bristolian\Middleware\ContentSecurityPolicyMiddleware;
 
 /**
- * @coversNothing
+ * @covers \Bristolian\Middleware\ContentSecurityPolicyMiddleware
  */
 class ContentSecurityPolicyMiddlewareTest extends BaseTestCase
 {
-    /**
-     * @covers \Bristolian\Middleware\ContentSecurityPolicyMiddleware
-     */
     public function testWorks()
     {
         $nonce = $this->injector->make(RequestNonce::class);
@@ -50,6 +47,45 @@ class ContentSecurityPolicyMiddlewareTest extends BaseTestCase
         $result = $middleware->process($request, $request_handler);
 
         $this->assertInstanceOf(ResponseInterface::class, $result);
-        // TODO - needs more assertions.
+    }
+
+    /**
+     * @group wip
+     */
+    public function testWorks_with_api_domain()
+    {
+        $nonce = $this->injector->make(RequestNonce::class);
+
+        $middleware = new ContentSecurityPolicyMiddleware(
+            $nonce,
+            ["https://api.bristolian.org"],
+            [],
+            []
+        );
+
+        $request = new ServerRequest();
+        $request_handler = new class() implements RequestHandler {
+            public function __construct()
+            {
+            }
+
+            public function handle(ServerRequestInterface $request): ResponseInterface
+            {
+                return new Response();
+            }
+        };
+
+        $result = $middleware->process($request, $request_handler);
+
+        $this->assertInstanceOf(ResponseInterface::class, $result);
+
+        $this->assertTrue($result->hasHeader("Content-Security-Policy"));
+
+        $header = $result->getHeader("Content-Security-Policy");
+        $this->assertCount(1, $header);
+        $this->assertStringContainsString(
+            "connect-src 'self' https://api.bristolian.org",
+            $header[0]
+        );
     }
 }
