0a7752e4693b3c8ae1db6cd8d823134988dc1585
Danack <Danack@basereality.com>
2026-02-13 23:40:00 +0000

    Differentiate debug window

--- containers/php_fpm/iterm-title.sh
+++ containers/php_fpm/iterm-title.sh
@@ -4,5 +4,9 @@ set_title() {
   printf '\033]0;%s\007' "$1"
 }
 
-set_title "bristolian php"
+if [ -n "${ITERM_TITLE:-}" ]; then
+  set_title "$ITERM_TITLE"
+else
+  set_title "bristolian php"
+fi
 trap 'set_title "iTerm – idle"' EXIT
