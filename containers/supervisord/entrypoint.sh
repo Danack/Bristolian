0dcc7f770c2a4a0965a1bdadda18a9d4ca3161a1
Danack <Danack@basereality.com>
2026-02-19 00:36:29 +0000

    Attempt to fix permissions issue.

--- containers/supervisord/entrypoint.sh
+++ containers/supervisord/entrypoint.sh
@@ -3,15 +3,9 @@ set -e
 set -x
 
 
-
 # There can be a race condition between the DB coming
 # up, and us trying to use it. Explicitly waiting for it
 # to be available prevents annoyance.
 php cli.php db:wait_for_db
 
-#/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 /usr/bin/supervisord -n -c /var/app/containers/supervisord/supervisord.conf
-
-
-#  /etc/supervisor/supervisord.conf
-# COPY tasks/*.conf /etc/supervisor/conf.d/
