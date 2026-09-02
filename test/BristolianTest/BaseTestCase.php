ca4178a539f3a7f004f12295433caf62ed740748
Danack <Danack@basereality.com>
2026-09-02 15:51:35 +0100

    Clean up code that is no longer needed as PHPUnit checks for bad output.

--- test/BristolianTest/BaseTestCase.php
+++ test/BristolianTest/BaseTestCase.php
@@ -32,45 +32,12 @@ class BaseTestCase extends TestCase
     public function setup(): void
     {
         $this->tiaSetUp();
-
         $this->injector = createTestInjector();
-        $this->startLevel = ob_get_level();
-        ob_start();
-
     }
 
     public function teardown(): void
     {
-        if ($this->skippedByTia() === true) {
-            return;
-        }
-
-        if ($this->startLevel === -1) {
-            $this->fail("startLevel was not set, cannot complete teardown");
-        }
-        $contents = ob_get_contents();
-        ob_end_clean();
-
-        $endLevel = ob_get_level();
-        $this->assertEquals($endLevel, $this->startLevel, "Mismatched ob_start/ob_end calls....somewhere");
-        $this->assertEquals(
-            0,
-            strlen($contents),
-            "Something has directly output to the screen: [".substr($contents, 0, 500)."]"
-        );
     }
-
-//    /**
-//     * @coversNothing
-//     */
-//    public function testPHPUnitApparentlyGetsConfused(): void
-//    {
-//        //Basically despite having:
-//        //<exclude>*/BaseTestCase.php</exclude>
-//        //in the phpunit.xml file it still thinks this file is a test class.
-//        //and then complains about it not having any tests.
-////        $this->assertTrue(true);
-//    }
 
     /**
      * @template T of object
